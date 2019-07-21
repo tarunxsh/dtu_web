cc8a7b23704137a1a596849948f476133ee7b582
tarunesh1234 <taruneshkumar1234@gmail.com>
2019-07-21 18:16:45 +0530

    foreignkey to_field used

--- campus/models.py
+++ campus/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
 	descp = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
-	author = models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True)
+	author = models.ForeignKey(User,on_delete=models.SET_NULL,db_column='author',to_field='username',blank=True,null=True)
 
 	def publish(self):
 		self.published_date=timezone.now()
